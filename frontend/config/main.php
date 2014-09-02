8dbf648248cb3e3d46d42311327e0f1a5c5ede50
Sammaye <sam.millman@googlemail.com>
2014-09-02 14:52:51 +0100

    Updated for new Yii2

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -16,6 +16,9 @@ return [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
         ],
+        'request' => [
+			'cookieValidationKey' => 'poop'
+		],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
