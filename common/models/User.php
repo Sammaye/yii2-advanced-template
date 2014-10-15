518d03e868383bfb9d0ba0c595d981332a033341
Sammaye <sam.millman@googlemail.com>
2014-10-15 16:37:04 +0100

    Added role usage to the user model

--- common/models/User.php
+++ common/models/User.php
@@ -26,7 +26,11 @@ class User extends ActiveRecord implements IdentityInterface
     const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
 
-    const ROLE_USER = 10;
+	const ROLE_USER = 'user';
+	const ROLE_AFFILIATE = 'affiliate';
+	const ROLE_STAFF = 'staff';
+	const ROLE_ADMIN = 'admin';
+	const ROLE_GOD = 'god';
 
     /**
      * @inheritdoc
